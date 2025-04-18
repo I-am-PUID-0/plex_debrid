fcc58484c4815d98aa32f9a674a29a33e571eadc
I-am-PUID-0 <36779668+I-am-PUID-0@users.noreply.github.com>
2025-04-18 16:54:40 +0000

    refactor(ui_print): revert to OG

--- ui/ui_print.py
+++ ui/ui_print.py
@@ -6,39 +6,38 @@ sameline = False
 sameline_log = False
 config_dir = "."
 
-def ui_cls(path='',update=""):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    logo(path=path,update=update)
 
-def logo(path='',update=""):
+def ui_cls(path='', update=""):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    logo(path=path, update=update)
+
+
+def logo(path='', update=""):
     print('                                                         ')
     print('           __                  __     __         _     __')
     print('    ____  / /__  _  __    ____/ /__  / /_  _____(_)___/ /')
     print('   / __ \/ / _ \| |/_/   / __  / _ \/ __ \/ ___/ / __  / ')
     print('  / /_/ / /  __/>  <    / /_/ /  __/ /_/ / /  / / /_/ /  ')
     print(' / .___/_/\___/_/|_|____\__,_/\___/_.___/_/  /_/\__,_/   ')
-    print('/_/               /_____/                         [v' + ui_settings.version[0] + ']' + update)
+    print(
+        '/_/               /_____/                         [v' + ui_settings.version[0] + ']' + update)
     print()
-    print('    ______________  __           __           __         ')
-    print('   / ____/ / __/ / / /___  _____/ /____  ____/ /         ')
-    print('  / __/ / / /_/ /_/ / __ \/ ___/ __/ _ \/ __  /          ')
-    print(' / /___/ / __/ __  / /_/ (__  ) /_/  __/ /_/ /           ')
-    print('/_____/_/_/ /_/ /_/\____/____/\__/\___/\__,_/            ')
-
 
     print(path)
     print()
     sys.stdout.flush()
 
+
 def set_log_dir(config):
     global config_dir
     config_dir = config
+
 
 def ui_print(string: str, debug="true"):
     global sameline
     global sameline_log
     try:
-        #log
+        # log
         if ui_settings.log == "true":
             try:
                 with open(config_dir + '/plex_debrid.log', 'a') as f:
@@ -50,21 +49,26 @@ def ui_print(string: str, debug="true"):
                         sameline_log = False
                     elif sameline_log and string.endswith('...'):
                         f.write('done' + '\n')
-                        f.write('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string  + ' ')
+                        f.write(
+                            '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + ' ')
                         sameline_log = True
                     elif string.endswith('...'):
-                        f.write('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + ' ')
+                        f.write(
+                            '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + ' ')
                         sameline_log = True
                     elif not string.startswith('done') and sameline_log:
                         f.write('done' + '\n')
-                        f.write('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + '\n')
+                        f.write(
+                            '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + '\n')
                         sameline_log = False
                     elif not string.startswith('done'):
-                        f.write('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + '\n')
+                        f.write(
+                            '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string + '\n')
                         sameline_log = False
             except:
-                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] logging error: couldnt write into log file at: ' + config_dir + '/plex_debrid.log')
-        #ui
+                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) +
+                      '] logging error: couldnt write into log file at: ' + config_dir + '/plex_debrid.log')
+        # ui
         if debug == "true":
             if string == 'done' and sameline:
                 print('done')
@@ -74,17 +78,21 @@ def ui_print(string: str, debug="true"):
                 sameline = False
             elif sameline and string.endswith('...'):
                 print('done')
-                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string, end=' ')
+                print(
+                    '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string, end=' ')
                 sameline = True
             elif string.endswith('...'):
-                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string, end=' ')
+                print(
+                    '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string, end=' ')
                 sameline = True
             elif not string.startswith('done') and sameline:
                 print('done')
-                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string)
+                print(
+                    '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string)
                 sameline = False
             elif not string.startswith('done'):
-                print('[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string)
+                print(
+                    '[' + str(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")) + '] ' + string)
                 sameline = False
             sys.stdout.flush()
     except:
